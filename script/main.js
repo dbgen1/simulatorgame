09b91c3b06c1c1e3f2bb1e3c95297673011047b9
Gen-DB <davitb2017@gmail.com>
2023-03-15 11:15:01 -0700

    change classes for JS/html

--- script/main.js
+++ script/main.js
@@ -1,66 +1,37 @@
-var upgrades = new Upgrades();
-
-document.getElementById("atomBtn").addEventListener("click", createAtoms);
-document.getElementById("ebsBtn").addEventListener("click", upgrades.purchase);
-document.getElementById("ucfBtn").addEventListener("click", upgrades.purchase);
-
-class Upgrades {
+class Game {
     constructor() {
-        this.upgradeElems = document.querySelectorAll('#upgrades p');
-        this.upgradeCosts = []
-        for (var i = 0; i < this.upgradeElems.length; i++) {
-            // get the cost of the upgrade (last element in the p tag)
-            var cost = parseInt(this.upgradeElems[i].lastElementChild.textContent);
-            this.upgradeCosts.push(cost);
+        this.upgrades = new Upgrades();
+        this.additive = 1;
+        this.multiplicative = 1;
 
-            this.upgradeElems[i].style.display = 'none';
+        this.createAtoms = this.createAtoms.bind(this);
+    }
+
+    updateAtoms(amount) {
+        var atomCountElem = document.getElementById("atomCount");
+        var atomCount = parseInt(atomCountElem.textContent);
+        atomCount += amount;
+        atomCountElem.textContent = atomCount;
+    
+        for (var i = 0; i < this.upgrades.costs.length; i++) {
+            if (atomCount > this.upgrades.costs[i] * 0.8) {
+                this.upgrades.setElementVisibility(i, true);
+            }
         }
     }
 
-    get costs() {
-        return this.upgradeCosts;
-    }
-
-    setElementVisibility(index, visibility) {
-        if (visibility) {
-            this.upgradeElems[index].style.display = 'block';
-        }
-        else {
-            this.upgradeElems[index].style.display = 'none';
-        }
-    }
-
-    purchase() {
-        var upgrade = this.parentElement;
-        var cost = parseInt(upgrade.lastElementChild.textContent);
-        if (atomCount >= cost) {
-            updateAtoms(-cost);
-        }
+    createAtoms() {
+        this.updateAtoms(Math.floor(Math.random() * 10) * this.multiplicative + this.additive);
     }
 }
 
-function createAtoms() {
-    updateAtoms(Math.floor(Math.random() * 10) + 1);
-}
 
-function updateAtoms(amount) {
-    var atomCountElem = document.getElementById("atomCount");
-    atomCount = parseInt(atomCountElem.textContent);
-    atomCount += amount;
-    atomCountElem.textContent = atomCount;
+var game = new Game();
 
-    for (var i = 0; i < upgrades.costs.length; i++) {
-        if (atomCount > upgrades.costs[i] * 0.8) {
-            upgrades.setElementVisibility(i, true);
-        }
-    }
-}
+document.getElementById("atomBtn").addEventListener("click", game.createAtoms);
+document.getElementById("ebsBtn").addEventListener("click", game.upgrades.purchase);
+document.getElementById("ucfBtn").addEventListener("click", game.upgrades.purchase);
 
-function updateButton(name, value) {
-    var button = document.getElementById(name);
-    button.textContent = value;
-}
 
-function getButton(name) {
-    return document.getElementById(name);
-}
+
+
